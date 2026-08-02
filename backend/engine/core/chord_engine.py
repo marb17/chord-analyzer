0ab909d9cca9c80c5b8bd80b35f2db1e92f82c07
marb17 <marvellchristiansutanto@gmail.com>
2026-08-02 14:49:28 +0800

    save me

--- backend/engine/core/chord_engine.py
+++ backend/engine/core/chord_engine.py
@@ -1,17 +1,150 @@
 from backend.engine.utils.classes.dataclass import Chord
+from backend.engine.utils.default.default_var import MUSICAL_INTERVALS
+from backend.engine.utils.functions.midi import midi_to_name
 
 
 class ChordEngine:
     def __init__(self):
         pass
 
-    def notes_to_chord(self, notes: list[int]) -> Chord:
-        sorted_notes = sorted(notes)
+    def notes_to_chord(self, notes: list[int]) -> list[Chord]:
+        no_dup_notes = sorted(list(set(notes)))
+
+        for base_note in no_dup_notes:
+            all_semitone_relative = [note - base_note for note in no_dup_notes]
+            pitch_classes = [note % 12 for note in all_semitone_relative]
+
+            all_intervals = list()
+            for note, abs_note in zip(all_semitone_relative, pitch_classes):
+                if note >= 0:
+                    all_intervals.append(MUSICAL_INTERVALS[note])
+                else:
+                    all_intervals.append(MUSICAL_INTERVALS[abs_note])
+
+            bass_note = no_dup_notes[0]
+            candidate_note = base_note
+
+            # --- THIRD / SUS DETECTION ---
+            has_sus2 = 2 in pitch_classes
+            has_minor_3rd = 3 in pitch_classes
+            has_major_3rd = 4 in pitch_classes
+            has_sus4 = 5 in pitch_classes
+
+            # --- FIFTH DETECTION ---
+            has_dim_5th = 6 in pitch_classes
+            has_perf_5th = 7 in pitch_classes
+            has_aug_5th = 8 in pitch_classes
+
+            # TRIAD DETECTION
+            is_major = 0.0
+            is_minor = 0.0
+            is_dim = 0.0
+            is_aug = 0.0
+            is_sus4 = 0.0
+            is_sus2 = 0.0
+
+            if has_sus2:
+                is_major += 0.0
+                is_minor += 0.0
+                is_dim += 0.0
+                is_aug += 0.0
+                is_sus4 += 0.0
+                is_sus2 += 0.5
+            if has_minor_3rd:
+                is_major += 0.0
+                is_minor += 1.0
+                is_dim += 0.5
+                is_aug += 0.0
+                is_sus4 += 0.0
+                is_sus2 += 0.0
+            if has_major_3rd:
+                is_major += 1.0
+                is_minor += 0.0
+                is_dim += 0.0
+                is_aug += 0.0
+                is_sus4 += 0.0
+                is_sus2 += 0.0
+            if has_sus4:
+                is_major += 0.0
+                is_minor += 0.0
+                is_dim += 0.0
+                is_aug += 0.0
+                is_sus4 += 0.5
+                is_sus2 += 0.0
+            if has_perf_5th:
+                is_major += 0.5
+                is_minor += 0.5
+                is_dim += 0.0
+                is_aug += 0.0
+                is_sus4 += 0.5
+                is_sus2 += 0.5
+            if has_dim_5th:
+                is_major += 0.0
+                is_minor += 0.0
+                is_dim += 0.5
+                is_aug += 0.0
+                is_sus4 += 0.0
+                is_sus2 += 0.0
+            if has_aug_5th:
+                is_major += 0.0
+                is_minor += 0.0
+                is_dim += 0.0
+                is_aug += 0.5
+                is_sus4 += 0.0
+                is_sus2 += 0.0
+
+            if has_major_3rd and has_perf_5th:
+                is_major += 2
+            elif has_minor_3rd and has_perf_5th:
+                is_minor += 2
+            elif has_minor_3rd and has_dim_5th:
+                is_dim += 2
+            elif has_major_3rd and has_aug_5th:
+                is_aug += 2
+            elif has_sus4 and has_perf_5th and not (has_major_3rd or has_minor_3rd):
+                is_sus4 += 2
+            elif has_sus2 and has_perf_5th and not (has_major_3rd or has_minor_3rd):
+                is_sus2 += 2
+
+            scores = {
+                "is_major": is_major,
+                "is_minor": is_minor,
+                "is_dim": is_dim,
+                "is_aug": is_aug,
+                "is_sus4": is_sus4,
+                "is_sus2": is_sus2,
+            }
+
+            best_quality_name = max(scores, key=scores.get)
+            highest_score = scores[best_quality_name]
+
+            print(f"Top Quality: {best_quality_name} ({highest_score} points)")
+
+            print(pitch_classes)
+            print(midi_to_name(candidate_note), midi_to_name(bass_note))
+            print(is_major, is_minor, is_dim, is_aug, is_sus4, is_sus2)
+            print([midi_to_name(note + base_note) for note in all_semitone_relative])
+            print()
+
+
+
+
+
+            
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
     engine = ChordEngine()
 
-    from backend.engine.utils.default.default_var import SIMPLE_MIDI_CHORDS
+    from backend.engine.utils.default.default_var import EXPANDED_MIDI_CHORDS
 
-    engine.notes_to_chord(SIMPLE_MIDI_CHORDS["C_Major"])
+    for key, value in EXPANDED_MIDI_CHORDS.items():
+        print(key)
+        engine.notes_to_chord(value)
+        print("--------------------------")
--- backend/engine/utils/default/default_var.py
+++ backend/engine/utils/default/default_var.py
@@ -15,34 +15,200 @@ PITCH_TO_NOTES_MAP = {
     11: ["B"],
 }
 
+MUSICAL_INTERVALS = {
+    0: "P1",
+    1: "m2",
+    2: "M2",
+    3: "m3",
+    4: "M3",
+    5: "P4",
+    6: "TT",
+    7: "P5",
+    8: "m6",
+    9: "M6",
+    10: "m7",
+    11: "M7",
+    12: "P8",
+    13: "m9",
+    14: "M9",
+    15: "m10",
+    16: "M10",
+    17: "P11",
+    18: "A11",
+    19: "P12",
+    20: "m13",
+    21: "M13",
+    22: "m14",
+    23: "M14",
+    24: "P15",
+}
+
 # TEST AREA
 
-SIMPLE_MIDI_CHORDS = {
-    # --- Basic Triads ---
-    "C_Major": [60, 64, 67],  # C4, E4, G4
-    "C_Minor": [60, 63, 67],  # C4, Eb4, G4
-    "D_Minor": [62, 65, 69],  # D4, F4, A4
-    "E_Minor": [64, 67, 71],  # E4, G4, B4
-    "F_Major": [53, 57, 60],  # F3, A3, C4
-    "G_Major": [55, 59, 62],  # G3, B3, D4
-    "A_Minor": [57, 60, 64],  # A3, C4, E4
-    "B_Diminished": [59, 62, 65],  # B3, D4, F4
-
-    # --- Suspended Triads ---
-    "C_Sus2": [60, 62, 67],  # C4, D4, G4
-    "C_Sus4": [60, 65, 67],  # C4, F4, G4
-
-    # --- Basic 7th Chords ---
-    "C_Major_7": [60, 64, 67, 71],  # C4, E4, G4, B4
-    "C_Dominant_7": [60, 64, 67, 70],  # C4, E4, G4, Bb4
-    "A_Minor_7": [57, 60, 64, 67],  # A3, C4, E4, G4
-    "D_Minor_7": [62, 65, 69, 72],  # D4, F4, A4, C5
-    "G_Dominant_7": [55, 59, 62, 65],  # G3, B3, D4, F4
-    "B_Half_Diminished_7": [59, 62, 65, 69],  # B3, D4, F4, A4
-
-    # --- Common Inversions & Open Voicings ---
-    "C_Major_1st_Inversion": [64, 67, 72],  # E4, G4, C5
-    "C_Major_2nd_Inversion": [55, 60, 64],  # G3, C4, E4
-    "G7_Drop_2": [55, 62, 65, 71],  # G3, D4, F4, B4
-    "A_Minor_7_Spread": [45, 60, 64, 67],  # A2, C4, E4, G4
+EXPANDED_MIDI_CHORDS = {
+    # --- BASIC TRIADS & INVERSIONS ---
+    "C_Major": [60, 64, 67],  # 1st: Root (C4, E4, G4)
+    "C_Major_1st_Inv": [64, 67, 72],  # 1st Inv: E4, G4, C5
+    "C_Major_2nd_Inv": [67, 72, 76],  # 2nd Inv: G4, C5, E5
+    "C_Minor": [60, 63, 67],  # Root: C4, Eb4, G4
+    "C_Minor_1st_Inv": [63, 67, 72],  # 1st Inv: Eb4, G4, C5
+    "C_Minor_2nd_Inv": [67, 72, 75],  # 2nd Inv: G4, C5, Eb5
+    "C_Diminished": [60, 63, 66],  # Root: C4, Eb4, Gb4
+    "C_Diminished_1st_Inv": [63, 66, 72],  # 1st Inv: Eb4, Gb4, C5
+    "C_Diminished_2nd_Inv": [66, 72, 75],  # 2nd Inv: Gb4, C5, Eb5
+    "C_Augmented": [60, 64, 68],  # Root: C4, E4, G#4
+    "C_Augmented_1st_Inv": [64, 68, 72],  # 1st Inv: E4, G#4, C5
+    "C_Augmented_2nd_Inv": [68, 72, 76],  # 2nd Inv: G#4, C5, E5
+    "C_Flat_5": [60, 64, 66],  # Root: C4, E4, Gb4
+    "C_Flat_5_1st_Inv": [64, 66, 72],  # 1st Inv: E4, Gb4, C5
+    "C_Flat_5_2nd_Inv": [66, 72, 76],  # 2nd Inv: Gb4, C5, E5
+    "C_Sus2": [60, 62, 67],  # Root: C4, D4, G4
+    "C_Sus2_1st_Inv": [62, 67, 72],  # 1st Inv: D4, G4, C5
+    "C_Sus2_2nd_Inv": [67, 72, 74],  # 2nd Inv: G4, C5, D5
+    "C_Sus4": [60, 65, 67],  # Root: C4, F4, G4
+    "C_Sus4_1st_Inv": [65, 67, 72],  # 1st Inv: F4, G4, C5
+    "C_Sus4_2nd_Inv": [67, 72, 77],  # 2nd Inv: G4, C5, F5
+    "C_Sus2_Add13": [60, 62, 67, 81],
+    "C_Sus4_Add13": [60, 65, 67, 81],
+    "C_Sus2_Add9": [60, 62, 67, 74],
+    # --- 7TH CHORDS & INVERSIONS ---
+    "C_Dominant_7": [60, 64, 67, 70],  # Root: C4, E4, G4, Bb4
+    "C_Dominant_7_1st_Inv": [64, 67, 70, 72],  # 1st Inv (C7/E): E4, G4, Bb4, C5
+    "C_Dominant_7_2nd_Inv": [67, 70, 72, 76],  # 2nd Inv (C7/G): G4, Bb4, C5, E5
+    "C_Dominant_7_3rd_Inv": [70, 72, 76, 79],  # 3rd Inv (C7/Bb): Bb4, C5, E5, G5
+    "C_Major_7": [60, 64, 67, 71],  # Root: C4, E4, G4, B4
+    "C_Major_7_1st_Inv": [64, 67, 71, 72],  # 1st Inv (Cmaj7/E): E4, G4, B4, C5
+    "C_Major_7_2nd_Inv": [67, 71, 72, 76],  # 2nd Inv (Cmaj7/G): G4, B4, C5, E5
+    "C_Major_7_3rd_Inv": [71, 72, 76, 79],  # 3rd Inv (Cmaj7/B): B4, C5, E5, G5
+    "C_Minor_7": [60, 63, 67, 70],  # Root: C4, Eb4, G4, Bb4
+    "C_Minor_7_1st_Inv": [63, 67, 70, 72],  # 1st Inv (Cm7/Eb): Eb4, G4, Bb4, C5
+    "C_Minor_7_2nd_Inv": [67, 70, 72, 75],  # 2nd Inv (Cm7/G): G4, Bb4, C5, Eb5
+    "C_Minor_7_3rd_Inv": [70, 72, 75, 79],  # 3rd Inv (Cm7/Bb): Bb4, C5, Eb5, G5
+    "C_Half_Diminished_7": [60, 63, 66, 70],  # Root: C4, Eb4, Gb4, Bb4
+    "C_Half_Diminished_7_1st_Inv": [63, 66, 70, 72],  # 1st Inv
+    "C_Half_Diminished_7_2nd_Inv": [66, 70, 72, 75],  # 2nd Inv
+    "C_Half_Diminished_7_3rd_Inv": [70, 72, 75, 78],  # 3rd Inv
+    "C_Diminished_7": [60, 63, 66, 69],  # Root: C4, Eb4, Gb4, A4
+    "C_Diminished_7_1st_Inv": [63, 66, 69, 72],  # 1st Inv
+    "C_Diminished_7_2nd_Inv": [66, 69, 72, 75],  # 2nd Inv
+    "C_Diminished_7_3rd_Inv": [69, 72, 75, 78],  # 3rd Inv
+    "C_Minor_Major_7": [60, 63, 67, 71],
+    "C_Minor_Major_7_1st_Inv": [63, 67, 71, 72],
+    "C_Minor_Major_7_2nd_Inv": [67, 71, 72, 75],
+    "C_Minor_Major_7_3rd_Inv": [71, 72, 75, 79],
+    "C_7_Sus4": [60, 65, 67, 70],
+    "C_7_Sus4_1st_Inv": [65, 67, 70, 72],
+    "C_7_Sus4_2nd_Inv": [67, 70, 72, 77],
+    "C_7_Sus4_3rd_Inv": [70, 72, 77, 79],
+    "C_9_Sus4": [60, 65, 67, 70, 74],
+    "C_13_Sus4": [60, 65, 67, 70, 74, 81],
+    "C_7_Sus4_Flat_9": [60, 65, 67, 70, 73],
+    # --- ADD CHORDS, 6THS & INVERSIONS ---
+    "C_Major_6": [60, 64, 67, 69],  # Root: C4, E4, G4, A4
+    "C_Major_6_1st_Inv": [64, 67, 69, 72],  # 1st Inv: E4, G4, A4, C5
+    "C_Major_6_2nd_Inv": [67, 69, 72, 76],  # 2nd Inv: G4, A4, C5, E5
+    "C_Major_6_3rd_Inv": [69, 72, 76, 79],  # 3rd Inv: A4, C5, E5, G5
+    "C_Minor_6": [60, 63, 67, 69],  # Root: C4, Eb4, G4, A4
+    "C_Minor_6_1st_Inv": [63, 67, 69, 72],  # 1st Inv: Eb4, G4, A4, C5
+    "C_Minor_6_2nd_Inv": [67, 69, 72, 75],  # 2nd Inv: G4, A4, C5, Eb5
+    "C_Minor_6_3rd_Inv": [69, 72, 75, 79],  # 3rd Inv: A4, C5, Eb5, G5
+    "C_69": [60, 64, 67, 69, 74],
+    "C_Add9": [60, 64, 67, 74],
+    "C_Add9_1st_Inv": [64, 67, 74, 72],
+    "C_Add9_2nd_Inv": [67, 74, 72, 76],
+    "C_Add11": [60, 64, 67, 77],
+    "C_Add13": [60, 64, 67, 81],
+    "C_Minor_Add9": [60, 63, 67, 74],
+    "C_Add9_Add11": [60, 64, 67, 74, 77],
+    "C_Add_Flat9_Add9": [60, 64, 67, 73, 74],
+    "C_7_Add9_Add13": [60, 64, 67, 70, 74, 81],
+    # --- CLEAN EXTENSIONS (9TH, 11TH, 13TH) & INVERSIONS ---
+    "C_Dominant_9": [60, 64, 67, 70, 74],  # Root: C4, E4, G4, Bb4, D5
+    "C_Dominant_9_1st_Inv": [
+        64,
+        67,
+        70,
+        74,
+        72,
+    ],  # 1st Inv (C9/E): E4, G4, Bb4, D5, C5
+    "C_Dominant_9_2nd_Inv": [
+        67,
+        70,
+        74,
+        72,
+        76,
+    ],  # 2nd Inv (C9/G): G4, Bb4, D5, C5, E5
+    "C_Dominant_9_3rd_Inv": [
+        70,
+        74,
+        72,
+        76,
+        79,
+    ],  # 3rd Inv (C9/Bb): Bb4, D5, C5, E5, G5
+    "C_Dominant_9_4th_Inv": [
+        74,
+        72,
+        76,
+        79,
+        82,
+    ],  # 4th Inv (C9/D): D5, C5, E5, G5, Bb5
+    "C_Dominant_11": [60, 64, 67, 70, 74, 77],
+    "C_Dominant_13": [60, 64, 67, 70, 74, 81],
+    "C_Minor_9": [60, 63, 67, 70, 74],
+    "C_Minor_9_1st_Inv": [63, 67, 70, 74, 72],
+    "C_Minor_9_2nd_Inv": [67, 70, 74, 72, 75],
+    "C_Minor_11": [60, 63, 67, 70, 74, 77],
+    "C_Minor_13": [60, 63, 67, 70, 74, 81],
+    "C_Major_9": [60, 64, 67, 71, 74],
+    "C_Major_9_1st_Inv": [64, 67, 71, 74, 72],
+    "C_Major_9_2nd_Inv": [67, 71, 74, 72, 76],
+    "C_Major_11": [60, 64, 67, 71, 74, 77],
+    "C_Major_13": [60, 64, 67, 71, 74, 81],
+    "C_Major_9_Add13": [60, 64, 67, 71, 74, 81],
+    "C_11_Add13": [60, 64, 67, 70, 74, 77, 81],
+    # --- ALTERED DOMINANTS & INVERSIONS ---
+    "C_7_Flat_9": [60, 64, 67, 70, 73],
+    "C_7_Flat_9_1st_Inv": [64, 67, 70, 73, 72],
+    "C_7_Sharp_9": [60, 64, 67, 70, 75],
+    "C_7_Sharp_9_1st_Inv": [64, 67, 70, 75, 72],
+    "C_7_Flat_5": [60, 64, 66, 70],
+    "C_7_Flat_5_1st_Inv": [64, 66, 70, 72],
+    "C_7_Sharp_5": [60, 64, 68, 70],
+    "C_7_Sharp_5_1st_Inv": [64, 68, 70, 72],
+    "C_7_Flat_9_Sharp_9": [60, 64, 67, 70, 73, 75],
+    "C_7_Sharp_9_Sharp_11": [60, 64, 67, 70, 75, 78],
+    "C_7_Flat_9_Sharp_11": [60, 64, 67, 70, 73, 78],
+    "C_7_Sharp_9_Flat_13": [60, 64, 67, 70, 75, 80],
+    "C_7_Flat_9_Flat_13": [60, 64, 67, 70, 73, 80],
+    "C_7_Sharp_9_Sharp_11_Flat_13": [60, 64, 67, 70, 75, 78, 80],
+    "C_7_Flat_5_Sharp_5": [60, 64, 66, 68, 70],
+    "C_7_Sharp_5_Sharp_11": [60, 64, 68, 70, 78],
+    # --- ADVANCED JAZZ & EXTENDED MONSTERS ---
+    "C_13_Flat_9_Sharp_11": [60, 64, 67, 70, 73, 78, 81],
+    "C_Major_13_Sharp_11": [60, 64, 67, 71, 74, 78, 81],
+    "C_Minor_9_Flat_5": [60, 63, 66, 70, 74],
+    "C_Major_9_Sharp_11": [60, 64, 67, 71, 74, 78],
+    "C_Minor_7_Flat_5_Flat_9": [60, 63, 66, 70, 73],
+    "C_Minor_7_Flat_5_Flat_9_Flat_13": [60, 63, 66, 70, 73, 80],
+    "C_7_Flat_9_Sharp_9_Sharp_11": [60, 64, 67, 70, 73, 75, 78],
+    "C_7_Sharp_9_Flat_13_Sharp_11": [60, 64, 67, 70, 75, 78, 80],
+    "C_13_Flat_9_Flat_13_Sharp_11": [60, 64, 67, 70, 73, 78, 80, 81],
+    "C_7_Altered_Full": [60, 64, 68, 70, 73, 75, 78, 80],
+    # --- DIMINISHED & MINOR/MAJOR EXTENSIONS ---
+    "C_Diminished_7_Add9": [60, 63, 66, 69, 74],
+    "C_Diminished_7_Flat_9": [60, 63, 66, 69, 73],
+    "C_Half_Diminished_7_Add11": [60, 63, 66, 70, 77],
+    "C_Minor_Major_9_Sharp_11": [60, 63, 67, 71, 74, 78],
+    # --- OMISSIONS & HARD-MODE TEST CASES ---
+    "C_Power_Chord": [60, 67],  # 1, 5 (Cno3)
+    "C_No5": [60, 64],  # 1, 3
+    "C_7_No5": [60, 64, 70],  # 1, 3, b7
+    "C_7_No5_1st_Inv": [64, 70, 72],  # E4, Bb4, C5
+    "C_9_No3": [60, 67, 70, 74],  # 1, 5, b7, 9
+    "C_13_No9": [60, 64, 67, 70, 81],  # 1, 3, 5, b7, 13
+    "C_7_No3_Flat_9": [60, 67, 70, 73],  # 1, 5, b7, b9
+    "C_13_No5_Sharp_11": [60, 64, 70, 74, 78, 81],  # 1, 3, b7, 9, #11, 13
+    "C_9_No3_No5": [60, 70, 74],  # 1, b7, 9
+    "C_7_Sus4_Add3": [60, 64, 65, 67, 70],  # 1, 3, 4, 5, b7
+    "C_FINAL_BOSS": [60, 64, 65, 70, 73, 75, 78, 80],
 }
